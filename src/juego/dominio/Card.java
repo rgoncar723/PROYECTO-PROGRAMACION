808df1ea1624e1cb97493bc35b2a22f4dfca3ef8
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-04-17 20:30:42 +0200

    Creacion de Card junto con sus métodos

--- src/juego/dominio/Card.java
+++ src/juego/dominio/Card.java
@@ -1,5 +1,27 @@
 package juego.dominio;
 
 public class Card {
-
+	private Rank rank;
+	private Suit suit;
+	
+	public Card (Rank rank, Suit suit) {
+		this.rank=rank;
+		this.suit=suit;
+	}
+	
+	public String getCardRank() {
+		return rank.toString();
+	}
+	public int getPoints() {
+		return rank.getRank();
+	}
+	public String getSuit() {
+		return suit.getDescription();
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s%s", rank,suit);
+	}
+	
 }
