65f295d930948cc5604c374d9d49d382cbda4696
rgoncar723 <rgoncar723@g.edcuaand.es>
2026-04-21 09:57:12 +0200

    Correcion de metodos de DiscardPile y Deck

--- src/juego/dominio/Deck.java
+++ src/juego/dominio/Deck.java
@@ -1,7 +1,9 @@
 package juego.dominio;
 
+import java.util.Collections;
 import java.util.Deque;
-import java.util.NoSuchElementException;
+import java.util.List;
+
 
 public class Deck   {
 	private Deque<Card> cards;
@@ -11,14 +13,24 @@ public class Deck   {
 		this.cards = DeckFactory.createCustomDeck(numberOfDecks);
 	}
 	
-	
-	public Card drawCard() {
-		if(cards.isEmpty()) {
-			
-			//Se llama a la lista de descarte...
-		}
-		
-		return cards.pop();
+	public void refillFromDiscard(DiscardPile discardPile) {
+	    List<Card> newCards = discardPile.grabAllButLast();
+	    Collections.shuffle(newCards); 
+	    for (Card card : newCards) {
+	        this.cards.push(card);
+	    }
+	}
+	public Card drawCard(DiscardPile discardPile) {
+	    if (cards.isEmpty()) {
+	        refillFromDiscard(discardPile);
+	    }
+	    
+	   
+	    if (cards.isEmpty()) {
+	        throw new IllegalStateException("No cards left in Deck or DiscardPile");
+	    }
+	    
+	    return cards.pop();
 	}
 	public int sizeDeck() {
 		return cards.size();
--- src/juego/dominio/DiscardPile.java
+++ src/juego/dominio/DiscardPile.java
@@ -4,32 +4,36 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class DiscardPile {
-	private Deque<Card> discardCards;
+	private Deque<Card> cards;
+
 	
 	public DiscardPile() {
-		this.discardCards = new ArrayDeque<>();
+		this.cards = new ArrayDeque<>();
 	}
 	public void push(Card card) {
-		discardCards.add(card);
+		cards.push(card);
 	}
-	public Card pop() {
-		if(discardCards.isEmpty()) {
-			throw new NoSuchElementException("Discard pile is empty...");
-		}
-		return discardCards.pop();
+	public Optional<Card> pop() {
+		return Optional.ofNullable(cards.poll());
 	}
-	public Card peek() {
-		return discardCards.peek();
+	public Optional<Card> peek() {
+        return Optional.ofNullable(cards.peek());
+    }
+	public boolean isEmpty() {
+		return cards.isEmpty();
 	}
-	
 	public List<Card> grabAllButLast(){
-		Card topCard = discardCards.pop();
-		List<Card> temporaryCards = new ArrayList<>(discardCards);
-		discardCards.clear();
-		discardCards.push(topCard);
+		Card topCard = cards.pop();
+		List<Card> temporaryCards = new ArrayList<>(cards);
+		
+		if(cards.size()<2) {
+			return temporaryCards;
+		}
+		cards.clear();
+		cards.push(topCard);
 		return temporaryCards;
 	}
 
